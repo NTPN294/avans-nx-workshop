3cd0f6b9960eeacc73864df6fd67731f8952bfb2
NTPN <ntpn294@gmail.com>
2024-11-13 14:56:23 +0100

    libs naming

--- libs/frontend/frontend-features/src/lib/frontend-features.module.ts
+++ libs/frontend/frontend-features/src/lib/frontend-features.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserDetailsComponent } from './users/user-details/user-details.component';
+import { UserListComponent } from './users/user-details/user-list.component';
+import { UserListComponent } from './users/user-list/user-list.component';
 
 @NgModule({
     imports: [CommonModule],
-    declarations: [UserDetailsComponent]
+    declarations: [UserDetailsComponent, UserListComponent, UserListComponent]
 })
 export class FrontendFeaturesModule {}
