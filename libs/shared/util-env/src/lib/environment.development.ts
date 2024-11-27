0679d8836d7672d472e474ba18b8b5dd3e49266a
NTPN <ntpn294@gmail.com>
2024-11-27 11:22:38 +0100

    env replace

--- libs/shared/util-env/src/lib/environment.development.ts
+++ libs/shared/util-env/src/lib/environment.development.ts
@@ -3,8 +3,8 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
     production: false,
 
-    ROOT_DOMAIN_URL: 'https://white-bay-0e232d503.5.azurestaticapps.net',
-    dataApiUrl: 'https://white-bay-0e232d503.5.azurestaticapps.net/api',
+    ROOT_DOMAIN_URL: 'http://localhost:3000',
+    dataApiUrl: 'http://localhost:3000/api',
 
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://ntpn294:Avans1234@ntpnavans.g3clv.mongodb.net/avans'
 };
--- libs/shared/util-env/src/lib/environment.ts
+++ libs/shared/util-env/src/lib/environment.ts
@@ -3,8 +3,8 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
     production: false,
 
-    ROOT_DOMAIN_URL: 'http://localhost:3000',
-    dataApiUrl: 'http://localhost:3000/api',
+    ROOT_DOMAIN_URL: 'dummy',
+    dataApiUrl: 'dummy',
 
-    MONGO_DB_CONNECTION_STRING: 'mongodb+srv://ntpn294:Avans1234@ntpnavans.g3clv.mongodb.net/avans'
+    MONGO_DB_CONNECTION_STRING: 'dummy'
 };
