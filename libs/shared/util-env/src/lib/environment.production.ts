dc935c9a583a6d099057e9cd9d0425bd167f9603
NTPN <ntpn294@gmail.com>
2024-11-27 10:09:45 +0100

    api

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -3,8 +3,8 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
     production: true,
 
-    ROOT_DOMAIN_URL: 'https://nxworkshop.azurewebsites.net',
-    dataApiUrl: 'https://nxworkshop.azurewebsites.net/api',
+    ROOT_DOMAIN_URL: 'https://white-bay-0e232d503.5.azurestaticapps.net',
+    dataApiUrl: 'https://white-bay-0e232d503.5.azurestaticapps.net/api',
 
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://ntpn294:Avans1234@ntpnavans.g3clv.mongodb.net/avans'
 };
