b40046ab579a97ce5e86af97eaea8f357533df6e
NTPN <ntpn294@gmail.com>
2024-11-28 12:15:22 +0100

    deploy3

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -3,8 +3,8 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
     production: true,
 
-    ROOT_DOMAIN_URL: 'https://3diceapi-f0cwe4d7fqgefxc8.scm.germanywestcentral-01.azurewebsites.net/',
-    dataApiUrl: 'https://3diceapi-f0cwe4d7fqgefxc8.scm.germanywestcentral-01.azurewebsites.net/api',
+    ROOT_DOMAIN_URL: 'https://3diceapi-f0cwe4d7fqgefxc8.germanywestcentral-01.azurewebsites.net',
+    dataApiUrl: 'https://3diceapi-f0cwe4d7fqgefxc8.germanywestcentral-01.azurewebsites.net/api',
 
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://ntpn294:Avans1234@ntpnavans.g3clv.mongodb.net/avans'
 };
