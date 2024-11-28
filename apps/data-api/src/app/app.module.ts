5003bfd87cd0c293acbb855ff89f3ba5474ae18f
NTPN <ntpn294@gmail.com>
2024-11-28 13:46:01 +0100

    hardcode db

--- apps/data-api/src/app/app.module.ts
+++ apps/data-api/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { Logger } from '@nestjs/common';
     imports: [
         BackendFeaturesMealModule,
         AuthModule,
-        MongooseModule.forRoot(environment.MONGO_DB_CONNECTION_STRING, {
+        MongooseModule.forRoot("mongodb+srv://ntpn294:Avans1234@ntpnavans.g3clv.mongodb.net/avans", {
             connectionFactory: (connection) => {
               connection.on('connected', () => {
                 Logger.verbose(
