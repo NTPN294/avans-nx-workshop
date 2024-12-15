aaa656606c28d71a9b9c2cc57bf3de61b1c57581
NTPN <ntpn294@gmail.com>
2024-12-15 09:24:01 +0100

    nestjs/config

--- apps/rcmnd-api/src/app/app.module.ts
+++ apps/rcmnd-api/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Neo4jBackendModule } from '@avans-nx-workshop/backend/neo4j';
 import { Module } from '@nestjs/common';
 import { Neo4jModule } from 'nest-neo4j/dist';
 import {environment} from '@avans-nx-workshop/shared/util-env'
-
+import {ConfigModule} from '@nestjs/config';
 @Module({
     imports: [
+        ConfigModule.forRoot({}),
         Neo4jModule.forRoot({
             scheme: 'bolt+s',
             host: environment.NEO4J_DB_HOST,
